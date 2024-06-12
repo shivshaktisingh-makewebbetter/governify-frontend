4cb48558d2f451342894267e9f3a17e927d155fa
shivshaktisingh-cedcommerce <shivshaktisingh@cedcommerce.com>
2024-06-12 14:57:35 +0530

    fixes

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -23,13 +23,37 @@ export const fetcher = async (endpoint , method , payload = null) =>{
 
 
 export const getRole = () =>{
-    let role = sessionStorage.getItem('role') || 'superAdmin';
-    return role;
+    let url = window.location.href;
+    let flag =  hasAdminKeyword(url);
+    if(flag){
+       return 'superAdmin';
+    }else{
+       return 'customer';
+    }
+    // let role = sessionStorage.getItem('role') || 'superAdmin';
+    // return role;
+}
+
+
+function hasAdminKeyword(url) {
+    if(url.includes('admin')){
+        return true;
+    }
+    return false;
 }
 
 
 
 const getToken = () =>{
-    let token = sessionStorage.getItem('token') || "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAiLCJpYXQiOjE3MTgxODIxNTIsImV4cCI6MTcxODE4NTc1MiwibmJmIjoxNzE4MTgyMTUyLCJqdGkiOiJiRk5UaVdDRGlMb3J3Z09aIiwic3ViIjoiMTEiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.ismpg70egW8yP9zEtltSOkcWsSAM9NGJgb7g8uzl2bI";
-    return token;
+    let url = window.location.href;
+    let user = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL29uYm9hcmRpZnkudGFzYzM2MC5jb20vY29tbW9tLWxvZ2luIiwiaWF0IjoxNzE4MTgzODgxLCJleHAiOjE3MTg0NDMwODEsIm5iZiI6MTcxODE4Mzg4MSwianRpIjoia1ZHSWE2anJ0dWN3eGN0VSIsInN1YiI6IjM0IiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.Gu9flNwjyVUtYkUvAs8FLxG2iw5thl5CMjRcShc0NxI';
+    let admin = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL29uYm9hcmRpZnkudGFzYzM2MC5jb20vY29tbW9tLWxvZ2luIiwiaWF0IjoxNzE4MTgzOTM3LCJleHAiOjE3MTg0NDMxMzcsIm5iZiI6MTcxODE4MzkzNywianRpIjoiMENqOFFJNEdGWWdJUTdLaSIsInN1YiI6IjciLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.7QpOSy8JNh1tKHOYSzp1lxb910QHKyFGtKQmUZ59pe4';
+    // let token = sessionStorage.getItem('token') || "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAiLCJpYXQiOjE3MTgxODIxNTIsImV4cCI6MTcxODE4NTc1MiwibmJmIjoxNzE4MTgyMTUyLCJqdGkiOiJiRk5UaVdDRGlMb3J3Z09aIiwic3ViIjoiMTEiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.ismpg70egW8yP9zEtltSOkcWsSAM9NGJgb7g8uzl2bI";
+   let flag =  hasAdminKeyword(url);
+
+   if(flag){
+    return admin;
+   }else{
+    return user;
+   }
 }
