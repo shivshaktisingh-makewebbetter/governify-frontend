77fd9174545c9e958047e08682b9f2ac42048e03
shivshaktisingh-cedcommerce <shivshaktisingh@cedcommerce.com>
2024-06-13 09:58:55 +0530

    fixes

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -6,7 +6,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { getRole } from "../../utils/helper";
 
 const Header = () => {
-	const data = JSON.parse(sessionStorage.getItem('settings')) || {"image":"https://onboardify.tasc360.com/uploads/governify/1717570622_Products Logo (1).png","site_bg":"#ffffff","button_bg":"#5ac063","banner_bg":"#5ac063","banner_content":"Hire an attitude, not just experience and qualification. Greg Savage.","header_bg":"#f7f7f7","head_title_color":"#5ac063"};
+	const data = JSON.parse(sessionStorage.getItem('settings')) || {"image":"https://onboardify.tasc360.com/uploads/governify/1718195689_Products%20Logo%20(1).png","site_bg":"#ffffff","button_bg":"#5ac063","banner_bg":"#5ac063","banner_content":"Hire an attitude, not just experience and qualification. Greg Savage.","header_bg":"#f7f7f7","head_title_color":"#5ac063"};
     const location = useLocation();
 	const navigate = useNavigate();
 	const [notification , setNotification] = useState(sessionStorage.getItem('notification_bar') || true);
