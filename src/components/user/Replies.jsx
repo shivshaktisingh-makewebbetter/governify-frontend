cf4e62cf4d1b81e3fdf9b9410c213cbb67e337a5
shivshaktisingh-cedcommerce <shivshaktisingh@cedcommerce.com>
2024-07-03 18:28:45 +0530

    fixes

--- src/components/user/Replies.jsx
+++ src/components/user/Replies.jsx
@@ -10,7 +10,7 @@ const Replies = ({ item }) => {
       <div>
         <span
           className="rounded-circle fw-bold text-white d-flex align-items-center justify-content-center"
-          style={{width: "35px", height: "35px" , background:"#5AC063" , paddingTop:"2px" }}
+          style={{width: "35px", height: "35px" , background:"#5AC063" , paddingTop:userName !== "Governify Team"?"2px":"0px" }}
         >
           {userName === "Governify Team" ? <GovernifyTeam /> : getFirstLettersOfName(userName)}
         </span>
