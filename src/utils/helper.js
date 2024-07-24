597c0e160fdf02b4bb071e138b8cfb4282d8aef9
shivshaktisingh-cedcommerce <shivshaktisingh@cedcommerce.com>
2024-07-24 14:28:09 +0530

    fixes

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -35,13 +35,13 @@ export const fetcher = async (endpoint, method, payload = null) => {
     const response = await fetch(url, requestOptions);
     const data = await response.json();
     console.log(data ,'unauthenticated');
-    if(data.hasOwnProperty('message') && data.message === 'Unauthenticated'){
+    if(data.hasOwnProperty('message') && data.message === 'Unauthenticated.'){
      window.location.href = 'https://governify.tasc360.com/signin';
     }
 
     return data;
   } catch (err) {
-    console.log(err ,'unauthenticated');
+    console.log(err ,'err');
   }
 };
 
